57a9bef5de038e7b3ca40e3b3225f926645e7694
Jéssica Moreira <100448388+JessicaMoreiraS@users.noreply.github.com>
2023-10-12 12:51:16 -0300

    Update script.js

--- public/JogoForca/js/script.js
+++ public/JogoForca/js/script.js
@@ -94,7 +94,7 @@ function criaLetras (sPalavra){
 
 function sorteia(){
     for(var m=0; m<sPerguntas.length; m++){
-        x= Math.random()* sPerguntas.length-1 + 0;
+        x= (int)(Math.random()* sPerguntas.length-1 + 0);
         console.log(x);
         iSorteados.push(m);   
        
